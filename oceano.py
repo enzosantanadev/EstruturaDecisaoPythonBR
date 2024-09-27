78d6b600ffce656296d817d0452990fd9a054d19
Enzo Santana <161235284+enzosantanadev@users.noreply.github.com>
2024-09-27 11:41:25 -0300

    Add files via upload

---
+++ oceano.py
@@ -0,0 +1,8 @@
+sexo = input("qual seu sexo? (responda em F ou M) ")
+
+if sexo.upper() == 'F':
+    print("seu sexo é feminino")
+elif sexo.upper() == 'M':
+    print("seu sexo é masculino")
+else:
+    print("sexo invalido")
