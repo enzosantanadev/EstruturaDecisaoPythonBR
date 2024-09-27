7a01e84c3b7b4a4d837fd2c502c530bd8f4dbc7a
Enzo Santana <161235284+enzosantanadev@users.noreply.github.com>
2024-09-27 11:22:09 -0300

    Add files via upload

---
+++ oceano.py
@@ -0,0 +1,11 @@
+a = float(input("digite um numero: "))
+b = float(input("digite outro numero: "))
+
+if a < b:
+    print(f"{b} é o maior deles")
+
+elif a > b:
+    print(f"{a} é o maior deles")
+
+else:
+    print("eles são iguais")
